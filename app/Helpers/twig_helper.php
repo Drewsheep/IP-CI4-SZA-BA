dddc9a5e64f3911a35b087b0b75c2d3f3e74d290
Drewsheep <145135162+Drewsheep@users.noreply.github.com>
2026-03-20 15:42:23 +0100

    Update twig_helper.php

--- app/Helpers/twig_helper.php
+++ app/Helpers/twig_helper.php
@@ -29,24 +29,13 @@ if (!function_exists('twig_conf')) {
             $twig->addExtension(new DebugExtension());
         }
 
-        if (!function_exists('langFilter')) {
-            function langFilter(string $file, string $key, array $params = []) {
-                if (!str_contains($key, '.')) {
-                    $key = $file . '.' . $key;
-                }
-                return lang($key, $params);
+        $twig->addFilter(new TwigFilter('t', function ($key, $params = []) {
+            if (!str_contains($key, '.')) {
+                $key = 'Lang.' . $key;
             }
-        }
 
-        $langFilters = [
-            't' => 'Lang'
-        ];
-
-        foreach ($langFilters as $filter => $file) {
-            $twig->addFilter(new TwigFilter($filter, function ($key, $params = []) use ($file) {
-                return langFilter($file, $key, $params);
-            }));
-        }
+            return lang($key, $params);
+        }));
 
 
         $twig->addFunction(new TwigFunction('asset', function ($path) {
@@ -59,7 +48,7 @@ if (!function_exists('twig_conf')) {
                 $version = time();
             }
 
-            return '/' . $path . '?v=' . $version;
+            return base_url($path) . '?v=' . $version;
         }));
 
         $twig->addFunction(new TwigFunction('current_url', fn() => current_url()));
